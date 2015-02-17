85c5fea3b43285e87b98f3f633660f590061ff66
Hexicube <marc@firstlinetech.co.uk>
2015-02-17 14:40:42 +0000

    Client now appears in middle of screen

--- src/org/tilegames/hexicube/cah/client/Client.java
+++ src/org/tilegames/hexicube/cah/client/Client.java
@@ -1,7 +1,9 @@
 package org.tilegames.hexicube.cah.client;
 
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Insets;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
@@ -70,6 +72,8 @@ public class Client
 				usernameConfirmed = true;
 			}
 		});
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setLocation(screenSize.width/2-150, screenSize.height/2-45);
 		frame.setVisible(true);
 		while(true)
 		{
